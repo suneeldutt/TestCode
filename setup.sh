ad8f9746cab1f9870e7d31b271d0a64edd95aac1
suneel <sdutt@cern.ch>
2016-01-12 09:07:26 +0100

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH=/afs/cern.ch/user/s/sdutt/work/ContactInteractions/external/bin:$PATH
+export LD_LIBRARY_PATH=/afs/cern.ch/user/s/sdutt/work/ContactInteractions/external/bin:$LD_LIBRARY_PATH
+export LHAPDF_DATA_PATH=/afs/cern.ch/user/s/sdutt/work/ContactInteractions/external/share/LHAPDF/PDFsets
+export LD_LIBRARY_PATH=/afs/cern.ch/user/s/sdutt/work/ContactInteractions/CMSCI/CI/lib:/afs/cern.ch/user/s/sdutt/work/ContactInteractions/CMSCI/CI/src:$LD_LIBRARY_PATH
